04a972caa89875f0bcf5f6682b6f41b69a8d2461
valentinlemarie <valentin.lemarie@wanadoo.fr>
2018-04-12 18:58:58 +0200

    objet calculatrice modifié bug fonctionnement main

---
+++ src/main/java/org/formation/java/Main.java
@@ -0,0 +1,35 @@
+package org.formation.java;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    final static String WORD_TO_QUIT = "=";
+
+
+    /*
+            @param
+            args:  program   arguments
+    */
+    public static void main(String args[]) {
+        Calculatrice calculatrice= new Calculatrice();
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter your operation and press Enter, ends with \"quit\" ");
+        boolean stop = false;
+        String inputStr = null;
+        while (!stop) {
+            if (keyboard.hasNextLine()) {
+                inputStr = keyboard.nextLine();
+                if (WORD_TO_QUIT.equals(inputStr)) {
+                    stop = true;
+                } else {
+                    calculatrice.Calculer(inputStr);
+                }
+            }
+        }
+        System.out.println(calculatrice.toString());
+    }
+}
+
